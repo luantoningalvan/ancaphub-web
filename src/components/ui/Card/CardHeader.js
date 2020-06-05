0101012a069e7c61e76ab2e44a79cb9a69c77548
Luan <ltoningalvan@gmail.com>
2020-06-04 23:13:48 -0300

    feat(cardheader): adds an action button option

--- src/components/ui/Card/CardHeader.js
+++ src/components/ui/Card/CardHeader.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { CardHeaderContainer } from './styles';
+import { Button } from '../Button';
 
-const CardHeader = ({ customContent, title, icon: Icon, ...rest }) => (
+const CardHeader = ({ customContent, title, action, ...rest }) => (
   <CardHeaderContainer {...rest}>
     {customContent ? (
       { customContent }
     ) : (
       <>
         <h3>{title}</h3>
-        {!!Icon && <Icon />}
+        {!!action && action.show && (
+          <Button onClick={() => action.action()} color="secondary">
+            {action.label}
+          </Button>
+        )}
       </>
     )}
   </CardHeaderContainer>
