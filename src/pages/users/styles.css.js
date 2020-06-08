438bd262ac27a7245434b882f18a651744d4461e
ViniFranco <email@vinifranco.com.br>
2020-06-08 17:21:58 -0300

    fix(users): add missing row gap
    Change column-gap to gap in order to match both Y and X axis

--- src/pages/users/styles.css.js
+++ src/pages/users/styles.css.js
@@ -4,7 +4,7 @@ export const UserCardGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-template-rows: auto;
-  column-gap: 16px;
+  gap: 16px;
   @media only screen and (max-width: 768px) {
     display: flex;
     flex-direction: column;
