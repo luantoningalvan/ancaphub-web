2e7c03f7015b73299ac924053455f975225ffcc7
Luan Tonin Galvan <ltoningalvan@gmail.com>
2020-10-13 19:05:56 -0300

    Posts module

--- src/api/posts.js
+++ src/api/posts.js
@@ -6,6 +6,6 @@ export const likePost = (postId) => axios.post(`/posts/${postId}/like`);
 export const getLikes = (postId) => axios.get(`/posts/${postId}/likes`);
 export const getFeedPosts = ({ currentPage = 1, pageSize = 5 }) =>
   axios.get('/posts/auth/feed', { params: { currentPage, pageSize } });
-export const getUserPosts = (data) => axios.get(`/posts/user/${data}`);
+export const getUserPosts = (data) => axios.get(`/timeline/profile/${data}`);
 export const votePostPoll = (data) =>
   axios.post(`/posts/${data.pollId}/vote`, { vote: data.option });
