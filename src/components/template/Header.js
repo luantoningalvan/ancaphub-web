aeb788e1106e9c7455d3c52d0707a4d531ac4632
Luan Tonin Galvan <ltoningalvan@gmail.com>
2020-10-12 23:26:19 -0300

    feat(notifications): add animation to the notifications icon

--- src/components/template/Header.js
+++ src/components/template/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { generate } from 'shortid';
 import { Link, useRouteMatch } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
@@ -8,13 +8,13 @@ import {
   FiSun as ContrastIcon,
   FiPower as LogoutIcon,
   FiChevronDown as ArrowDownIcon,
-  FiBell as NotificationsIcon,
   FiSettings as SettingsIcon,
   FiMenu as MenuIcon,
   FiMessageCircle as MessageIcon,
 } from 'react-icons/fi';
 
 import { useDispatch, useSelector } from 'react-redux';
+import styled, { css } from 'styled-components';
 import {
   Dropdown,
   DropdownListContainer,
@@ -38,10 +38,138 @@ import Notification from '../notifications';
 import { logoutRequest as logout } from '../../actions/auth';
 import { switchColorMode as changeTheme } from '../../actions/settings';
 import { ReactComponent as AncapHubLogo } from '../../assets/ancaphub.svg';
+import { ReactComponent as AnimatedBell } from '../../assets/bell.svg';
+
+import notificationSound from '../../assets/notification.mp3';
+
+const Bell = styled.div`
+  #e8n1zamhvyle3_tr {
+    transform: translate(12px, 9.5px) rotate(0deg);
+  }
+
+  #e8n1zamhvyle5_to {
+    transform: translate(12px, 21.503926px);
+  }
+
+  ${(props) =>
+    props.animated &&
+    css`
+      #e8n1zamhvyle3_tr {
+        animation: e8n1zamhvyle3_tr__tr 3000ms linear 1 normal forwards;
+      }
+
+      @keyframes e8n1zamhvyle3_tr__tr {
+        0% {
+          transform: translate(12px, 9.5px) rotate(10deg);
+          animation-timing-function: cubic-bezier(0, 0, 0.58, 1);
+        }
+        3.333333% {
+          transform: translate(12px, 9.5px) rotate(-10deg);
+          animation-timing-function: cubic-bezier(0, 0, 0.58, 1);
+        }
+        6.666667% {
+          transform: translate(12px, 9.5px) rotate(10deg);
+          animation-timing-function: cubic-bezier(0, 0, 0.58, 1);
+        }
+        10% {
+          transform: translate(12px, 9.5px) rotate(-10deg);
+          animation-timing-function: cubic-bezier(0, 0, 0.58, 1);
+        }
+        13.333333% {
+          transform: translate(12px, 9.5px) rotate(10deg);
+          animation-timing-function: cubic-bezier(0, 0, 0.58, 1);
+        }
+        15% {
+          transform: translate(12px, 9.5px) rotate(0deg);
+        }
+        100% {
+          transform: translate(12px, 9.5px) rotate(0deg);
+        }
+      }
+      #e8n1zamhvyle5_to {
+        animation: e8n1zamhvyle5_to__to 3000ms linear 1 normal forwards;
+      }
+      @keyframes e8n1zamhvyle5_to__to {
+        0% {
+          transform: translate(17px, 21.503926px);
+          animation-timing-function: cubic-bezier(0, 0, 0.58, 1);
+        }
+        3.333333% {
+          transform: translate(7px, 21.503926px);
+          animation-timing-function: cubic-bezier(0, 0, 0.58, 1);
+        }
+        6.666667% {
+          transform: translate(17px, 21.503926px);
+          animation-timing-function: cubic-bezier(0, 0, 0.58, 1);
+        }
+        10% {
+          transform: translate(7px, 21.503926px);
+          animation-timing-function: cubic-bezier(0, 0, 0.58, 1);
+        }
+        13.333333% {
+          transform: translate(17px, 21.503926px);
+          animation-timing-function: cubic-bezier(0, 0, 0.58, 1);
+        }
+        15% {
+          transform: translate(12px, 21.503926px);
+        }
+        100% {
+          transform: translate(12px, 21.503926px);
+        }
+      }
+      #e8n1zamhvyle5_tr {
+        animation: e8n1zamhvyle5_tr__tr 3000ms linear 1 normal forwards;
+      }
+      @keyframes e8n1zamhvyle5_tr__tr {
+        0% {
+          transform: rotate(0deg);
+          animation-timing-function: cubic-bezier(0, 0, 0.58, 1);
+        }
+        3.333333% {
+          transform: rotate(5deg);
+          animation-timing-function: cubic-bezier(0, 0, 0.58, 1);
+        }
+        6.666667% {
+          transform: rotate(0deg);
+          animation-timing-function: cubic-bezier(0, 0, 0.58, 1);
+        }
+        10% {
+          transform: rotate(5deg);
+          animation-timing-function: cubic-bezier(0, 0, 0.58, 1);
+        }
+        13.333333% {
+          transform: rotate(-5deg);
+          animation-timing-function: cubic-bezier(0, 0, 0.58, 1);
+        }
+        15% {
+          transform: rotate(0deg);
+        }
+        100% {
+          transform: rotate(0deg);
+        }
+      }
+    `}
+`;
+const audio = new Audio(notificationSound);
 
 const Header = ({ user, collapsed, setCollapsed }) => {
   const { url } = useRouteMatch();
   const dispatch = useDispatch();
+  const [animated, setAnimated] = useState(false);
+
+  const escFunction = (event) => {
+    if (event.keyCode === 16) {
+      audio.play();
+      setAnimated(true);
+
+      setTimeout(() => {
+        setAnimated(false);
+      }, 1000);
+    }
+  };
+
+  document.addEventListener('keydown', escFunction);
+
   const { notifications, notReadCount } = useSelector(
     (state) => state.notifications
   );
@@ -89,7 +217,9 @@ const Header = ({ user, collapsed, setCollapsed }) => {
             toggle={
               <HeaderMenuItem current={url.includes('/notifications')}>
                 <div>
-                  <NotificationsIcon />
+                  <Bell animated={animated}>
+                    <AnimatedBell />
+                  </Bell>
                   {notReadCount > 0 && <span className="badge" />}
                 </div>
               </HeaderMenuItem>
