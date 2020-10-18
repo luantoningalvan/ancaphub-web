51825483b04105f23daa817ca2bcc5ea8135590e
Luan Tonin Galvan <ltoningalvan@gmail.com>
2020-10-17 23:15:59 -0300

    Convert UserAvatar to typescript

---
+++ src/components/users/UserAvatar/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Avatar } from './styles';
+
+import defaultAvatar from '../../../assets/default-profile-picture.jpg';
+
+interface UserAvatarProps {
+  user: {
+    id: string;
+    avatar: string;
+    username: string;
+  };
+  size?: number;
+  style?: React.CSSProperties;
+}
+
+const UserAvatar: React.FC<UserAvatarProps> = ({
+  user,
+  style,
+  size,
+  ...props
+}) => (
+  <Avatar size={size || 35} {...props}>
+    <Link to={`/${user.username}`}>
+      <img
+        src={user.avatar && user.avatar !== '' ? user.avatar : defaultAvatar}
+        alt={user.username}
+      />
+    </Link>
+  </Avatar>
+);
+
+export default UserAvatar;
