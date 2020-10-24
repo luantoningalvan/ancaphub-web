5b5651c6db96e55e5eabd771e737dd0ba2d0b27f
Luan Tonin Galvan <ltoningalvan@gmail.com>
2020-10-24 16:13:47 -0300

    Convert "Trends" components to typescript

--- src/components/trends/TrendingTopicsWidget/index.tsx
+++ src/components/trends/TrendingTopicsWidget/index.tsx
@@ -1,35 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
 import { FormattedMessage } from 'react-intl';
-
-import { Card, CardHeader, CardBody, CardFooter } from '../ui';
-
-const TrendingList = styled.ul`
-  li {
-    margin-bottom: 15px;
-    list-style: none;
-    border-bottom: 1px solid ${(props) => props.theme.palette.border};
-    padding-bottom: 15px;
-
-    &:last-child {
-      padding: 0;
-      margin: 0;
-      border: none;
-    }
-  }
-
-  h4 {
-    font-weight: bold;
-    font-size: 1.1em;
-    color: ${(props) => props.theme.palette.text.primary};
-  }
-
-  span {
-    color: ${(props) => props.theme.palette.text.secondary};
-    font-size: 0.9em;
-    font-weight: lighter;
-  }
-`;
+import { Card, CardHeader, CardBody, CardFooter } from 'snake-ui';
+import { TrendingList } from './styles';
 
 const TrendingTopicsWidget = () => (
   <div style={{ width: '100%' }}>
